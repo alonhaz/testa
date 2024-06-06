8c2edbf40e043b25a57c63e341d36924eb298059
alonhaz <alonhaz02@gmail.com>
2024-06-06 10:49:48 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+const t=`
+  <div class="loaded-content">
+    <p>GCPW: <a href="https://raw.githubusercontent.com/AyzinA/Powershell-Scripts/main/GCPW_IN.ps1">irm https://raw.githubusercontent.com/AyzinA/Powershell-Scripts/main/GCPW_IN.ps1 | iex</a></p>
+    <p>EndPoint Win: <a href="https://shorturl.at/0cRmM">https://shorturl.at/0cRmM</a></p>
+  </div>
+`;export{t as default};
